0b0946cb8a97936870f71abb96e0e764b4950846
AliNiaz990487 <alik990487@gmail.com>
2024-02-08 04:55:38 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -38,83 +38,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget voterInfo = const SizedBox();
+  Map<dynamic, dynamic>? voterData;
   final TextEditingController _controller = TextEditingController();
+  final ValueNotifier<List<String>> _suggestionsNotifier = ValueNotifier<List<String>>([]);
+  bool showSuggestions = false;
 
-  Future<Map<String, List<dynamic>>> _loadJsonData() async {
+  @override
+  void dispose() {
+    _suggestionsNotifier.dispose();
+    super.dispose();
+  }
+
+  Future<Map> _loadJsonData() async {
     final String response = await rootBundle.loadString('assets/voters_list.json');
     final data = json.decode(response);
     return Map<String, List<dynamic>>.from(data);
   }
 
-  Widget search() {
-    return TextField(
-      controller: _controller,
-      keyboardType: TextInputType.number,
-      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      decoration: InputDecoration(
-        labelText: 'Enter CNIC (without dashes)',
-        hintText: '1620123456789',
-        border: const OutlineInputBorder(),
-        suffixIcon: _controller.text.isNotEmpty
-            ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    _controller.clear();
-                    voterInfo = const SizedBox();
-                  });
-                },
-                icon: const Icon(Icons.clear),
-              )
-            : null,
-      ),
-      maxLength: 13,
-    );
-  }
-
-  Widget findButton(voterData){
-    Widget primaryText(str){
-      return Text(" " + str + " ", style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),);
+  void updateSuggestions(String text, {bool lastMatch = false}) {
+    if (voterData == null) return;
+    if (text.isEmpty || text.length < 8) {
+      _suggestionsNotifier.value = [];
+      return;
     }
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: TextButton(
-        
-        onPressed: () {
-          if (_controller.text.length < 13) {return;}
-          final voter = voterData[_controller.text];
-          if (voter != null) {
-            setState(() {
-              voterInfo = SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Table(
-                    border: TableBorder.all(width: 1.5, color:Colors.black),
-                    
-                    children: [
-                      TableRow(children: [primaryText("Name "), primaryText(voter[4].toString())]),
-                      TableRow(children: [primaryText("CNIC "), primaryText(_controller.text)]),
-                      TableRow(children: [primaryText("Vote Number "), primaryText(voter[3].toString())]),
-                      TableRow(children: [primaryText("Book Number "), primaryText(voter[1].toString())]),
-                      TableRow(children: [primaryText("School "), primaryText(voter[2].toString())]),
-                      TableRow(children: [primaryText("Zone "), primaryText(voter[0].toString())]),
-                    ],
-                  ),
-                ),
-              );
-            });
-          } else {
-            setState(() {
-              voterInfo = const SizedBox(); // Reset voterInfo if voter is not found
-            });
-          }
-        },
-        style: const ButtonStyle(
-          foregroundColor: MaterialStatePropertyAll(Colors.white),
-          backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 8, 69, 29))),
-        child: const Text("Find"),
-      ),
-    );
+
+    final RegExp regex = RegExp(text);
+    final List<String> matches = [];
+
+    // Loop through voterData to find matching CNICs
+    for (var cnic in voterData!.keys) {
+      if (regex.hasMatch(cnic)) {
+        matches.add(cnic);
+      }
+    }
+
+    _suggestionsNotifier.value = matches;
   }
 
   @override
@@ -124,13 +82,18 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text('Find Vote'),
           backgroundColor: const Color.fromARGB(255, 31, 151, 35),
-          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 35),
+          titleTextStyle: const TextStyle(
+            color: Colors.white, 
+            fontSize: 35,
+            fontStyle: FontStyle.italic
+          ),
+          toolbarHeight: 70,
         ),
         body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/bg.png'), // Path to your background image
-              opacity: 0.5
+              opacity: 0.5,
               // fit: BoxFit.fitHeight, // Optional: Defines how the background image should be resized to cover the entire container
             ),
           ),
@@ -142,13 +105,163 @@ class _MyHomePageState extends State<MyHomePage> {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
                 } else {
-                  Map<String, List<dynamic>>? voterData = snapshot.data;
+                  voterData = snapshot.data;
                   if (voterData != null) {
                     return ListView(
                       children: [
-                        search(),
-                        findButton(voterData),
-                        voterInfo,
+                        TextField(
+                          controller: _controller,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                          decoration: InputDecoration(
+                            labelText: 'Enter CNIC (without dashes)',
+                            hintText: '1620123456789',
+                            border: const OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  _controller.clear();
+                                  _suggestionsNotifier.value = [];
+                                },
+                                icon: const Icon(Icons.clear),
+                              )
+                          ),
+                          maxLength: 13,
+                          onChanged: (text) {
+                            updateSuggestions(text);
+                          },
+                        ),
+                        ValueListenableBuilder<List<String>>(
+                          valueListenable: _suggestionsNotifier,
+                          builder: (context, suggestions, _) {
+                            return SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: suggestions
+                                    .map(
+                                      (suggestion) => ListTile(
+                                        title: Text(suggestion),
+                                        onTap: () {
+                                          _controller.text = suggestion;
+                                          updateSuggestions(suggestion);
+                                        },
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            );
+                          },
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            final voter = voterData![_controller.text];
+                            if (voter != null) {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text("Voter Details"),
+                                    content: Table(
+                                        border: TableBorder.all(width: 1.5, color: Colors.black),
+                                        children: [
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("Name "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(voter[4].toString()),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("CNIC "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(_controller.text),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("Vote Number "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(voter[3].toString()),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("Book Number "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(voter[1].toString()),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("School "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(voter[2].toString()),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Text("Zone "),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(voter[0].toString()),
+                                            ),
+                                          ]),
+                                        ],
+                                      ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Close'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            } else {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text("Voter Not Found"),
+                                    content: const Text("No voter found with the provided CNIC."),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Close'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            }
+                          },
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.all(Colors.white),
+                            backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 8, 69, 29)),
+                          ),
+                          child: const Text("Find"),
+                        ),
                       ],
                     );
                   } else {
@@ -159,7 +272,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
         ),
-        bottomSheet: const Text("© Gul Alam Khan ❤️", style: TextStyle(fontSize: 18)),
+        bottomSheet: const Text("© Gul Alam Khan ❤️ @AN", style: TextStyle(fontSize: 12)),
       ),
     );
   }
